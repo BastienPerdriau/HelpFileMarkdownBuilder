cf15c6efaf7c3129ff24f3582fdbc91a5d406c7f
Bastien Perdriau <bastien.perdriau@live.fr>
2018-08-26 00:00:50 +0200

    Adding ProjectInfo and BuildConfiguration

---
+++ Sources/HelpFileMarkdownBuilder.CSharp.Builder/BuildConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace HelpFileMarkdownBuilder.CSharp.Builder
+{
+    /// <summary>
+    /// Build configuration
+    /// </summary>
+    public class BuildConfiguration
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Output path
+        /// </summary>
+        public string OutputPath { get; set; }
+
+        /// <summary>
+        /// Documentation file path
+        /// </summary>
+        public string DocumentationFilePath { get; set; }
+    }
+}
---
+++ Sources/HelpFileMarkdownBuilder.CSharp.Builder/ProjectInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HelpFileMarkdownBuilder.CSharp.Builder
+{
+    /// <summary>
+    /// Project informations
+    /// </summary>
+    public class ProjectInfo
+    {
+        /// <summary>
+        /// Project file path
+        /// </summary>
+        public string ProjectFilePath { get; set; }
+
+        /// <summary>
+        /// Assembly name
+        /// </summary>
+        public string AssemblyName { get; set; }
+
+        /// <summary>
+        /// Build configurations
+        /// </summary>
+        public Dictionary<string, BuildConfiguration> BuildConfigurations = new Dictionary<string, BuildConfiguration>();
+    }
+}
