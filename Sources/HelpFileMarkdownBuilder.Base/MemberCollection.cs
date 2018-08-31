2fa4eb555711f4843a30b0f33a1d46c10cefdf44
Bastien Perdriau <bastien.perdriau@live.fr>
2018-08-31 21:52:42 +0200

    Adding MemberCollection

---
+++ Sources/HelpFileMarkdownBuilder.Base/MemberCollection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HelpFileMarkdownBuilder.Base
+{
+    /// <summary>
+    /// Collection of members
+    /// </summary>
+    public class MemberCollection<T> : List<T> where T : Member
+    {
+
+    }
+}
