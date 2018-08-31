5aa782f145606d417f634477a34348b051e1d7b0
Bastien Perdriau <bastien.perdriau@live.fr>
2018-08-31 21:55:03 +0200

    Adding CSMemberCollection

---
+++ Sources/HelpFileMarkdownBuilder.CSharp.Members/CSMemberCollection.cs
@@ -0,0 +1,13 @@
+﻿using HelpFileMarkdownBuilder.Base;
+
+namespace HelpFileMarkdownBuilder.CSharp.Members
+{
+    /// <summary>
+    /// Collection of CSMembers
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CSMemberCollection<T> : MemberCollection<T> where T : CSMember
+    {
+
+    }
+}
